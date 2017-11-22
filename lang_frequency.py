6cf544281ef1fafc9b5e68d367ef107fa26991d0
Igor Statkevich <MadInc63@gmail.com>
2017-11-22 23:52:41 +0400

    fix misprint fix splitting strings with backslash rename text_from_book to text_from_file rename dict_of_most_frequent_words to tuple_of_most_frequent_words

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,7 +3,7 @@ import argparse
 from collections import Counter
 
 
-def creat_parser():
+def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("file_path",
                         help="file path to find top ten words")
@@ -12,8 +12,8 @@ def creat_parser():
 
 def load_data(file_path):
     with open(file_path, 'r') as text_file:
-        text_from_book = text_file.read()
-    return text_from_book
+        text_from_file = text_file.read()
+    return text_from_file
 
 
 def convert_to_lowercase(normal_text):
@@ -21,22 +21,21 @@ def convert_to_lowercase(normal_text):
 
 
 def get_most_frequent_words(text):
-    found_words_in_text = re.findall(r'\w+', text)
-    number_of_most_common_words = 10
-    most_common_words = Counter(found_words_in_text)\
-                        .most_common(number_of_most_common_words)
+    found_words = re.findall(r'\w+', text)
+    number_of_output_word = 10
+    most_common_words = Counter(found_words).most_common(number_of_output_word)
     return most_common_words
 
 
 if __name__ == '__main__':
     try:
-        file_path = creat_parser().file_path
+        file_path = create_parser().file_path
         load_text_from_file = load_data(file_path)
         lowercase_text = convert_to_lowercase(load_text_from_file)
-        dict_of_most_frequent_words = get_most_frequent_words(lowercase_text)
+        tuple_of_most_frequent_words = get_most_frequent_words(lowercase_text)
         print('Ten most repetitive words in the text and the number of their'
               ' repetitions:')
-        for word, count in dict_of_most_frequent_words:
+        for word, count in tuple_of_most_frequent_words:
             print('"{}" = {}'.format(word, count))
     except (IOError, FileNotFoundError):
         print('No such file or directory')
