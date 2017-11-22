965b5e8c139b6446d5b21f821c6ebd9d80ee3b7c
MadInc63 <MadInc63@gmail.com>
2017-11-22 09:41:26 +0300

    fix

--- lang_frequency.py
+++ lang_frequency.py
@@ -13,22 +13,30 @@ def parser_arg():
 
 def load_data(file_path):
     with open(file_path, 'r') as text_file:
-        lowercase_text = text_file.read().lower()
+        text_from_book = text_file.read()
+    return text_from_book
+
+
+def convert_to_lowercase(normal_text):
+    lowercase_text = normal_text.lower()
     return lowercase_text
 
 
 def get_most_frequent_words(text):
-    words_in_text = re.findall(r'\w+', text)
-    words_count = Counter(words_in_text).most_common(10)
-    return words_count
-
+    found_words_in_text = re.findall(r'\w+', text)
+    word_counting_in_text = Counter(found_words_in_text).most_common(10)
+    return word_counting_in_text
 
 
 if __name__ == '__main__':
     try:
         file_path = parser_arg()
-        text_from_file = load_data(file_path)
-        top_10_words = get_most_frequent_words(text_from_file)
-        print(top_10_words)
+        load_text_from_file = load_data(file_path)
+        text_to_lowercase = convert_to_lowercase(load_text_from_file)
+        most_frequent_words = get_most_frequent_words(text_to_lowercase)
+        for most_word in most_frequent_words:
+            word, count = most_word
+            print('The word "{}" occurs {} times in text'
+                  .format(word, count))
     except (IOError, FileNotFoundError):
         print('No such file or directory')
